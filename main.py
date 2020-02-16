5e1ea4f77160ef57667550991a53c94c3870e204
Alex Silence <asilence@uvm.edu>
2020-02-16 13:31:54 -0500

    Created method outline

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 def main():
     print("Program start")
 
+# get all songs by artist
+def getArtist(artist):
+    print("artist")    
+
+# rank, times streamed, track id
+def getSong(song):
+    pritn("song")
+
+# get top x songs
+def getTopX(x):
+    print("top x")
+
+
 main()

2fd5804c0be665cf5ec7fdfbf9223ce5ab55fc74
Mateo Riofrio <mriofrio@uvm.edu>
2020-02-18 17:34:03 -0500

    add print_table, create_connection methods

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-validQueries = ['help', 'song', 'artist', 'quit', 'load_database']
+import createDatabase
+import sqlite3 as lite
+
+validQueries = ['help', 'song', 'artist', 'quit']
+db_file = "spotifyDatabase.db"
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+            specified by the db_file
+        :param db_file: database file
+        :return: Connection object or None
+        """
+
+    conn = lite.connect(db_file)
+    return conn
+
+
+def print_table(conn, tableName):
+    """
+       Query all rows in the given table
+       :param conn: the Connection object
+       :return:
+       """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM " + tableName)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
 
 def main():
     print("Welcome to the spotify database searching tool, for help type 'help'")
-    
+
     query = input(">> ")
     query = query.split()
     queryType = query[0]
@@ -13,7 +44,7 @@ def main():
             query = input(">> ")
             query = query.split()
             queryType = query[0]
-    
+
         if queryType == "quit":
             print("Thank you for using our program")
         else:
@@ -23,28 +54,25 @@ def main():
                 getSong(query[1])
             elif queryType == "artist":
                 getArtist(query[1])
-            elif queryType == "load_database":
-                print("TODO: load database")
-            
+
             query = input(">> ")
             query = query.split()
             queryType = query[0]
-    
+
+
 # get all songs by artist
 def getArtist(artist):
     print(artist)
 
+
 # rank, times streamed, track id
 def getSong(song):
-    print(song)
+    createDatabase.getSong(song)
+
 
 # get top x songs
 def getTopX(x):
     print("top " + x)
-
-#get top song of one artist
-def getTopSong(artist):
-    print(artist)
 
 
 main()
